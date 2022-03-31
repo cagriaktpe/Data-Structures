b6bde3c569cd822e1b8190d93ceed591d600bacd
Samet Çağrı AKTEPE <blackorsaktepe@gmail.com>
2022-03-31 14:25:37 +0300

    Queue backed Array

--- src/com/sametcagriaktepe/Main.java
+++ src/com/sametcagriaktepe/Main.java
@@ -12,19 +12,23 @@ public class Main {
         Employee donaldTrump = new Employee("Donald", "Trump", 5);
         Employee joeBiden = new Employee("Joe", "Biden", 7);
 
-        LinkedStack stack = new LinkedStack();
+        ArrayQueue queue = new ArrayQueue(10);
 
-        stack.push(janeJones);
-        stack.push(frankCastle);
-        stack.push(joeBiden);
+        queue.add(janeJones);
+        queue.add(frankCastle);
+        queue.add(georgeWashington);
+        queue.add(barackObama);
+        queue.add(donaldTrump);
+        queue.add(joeBiden);
 
-        stack.printStack();
+        queue.printQueue();
 
         System.out.println("====================");
+        queue.remove();
+        queue.printQueue();
 
-        stack.pop();
-
-        stack.printStack();
+        System.out.println("====================");
+        System.out.println(queue.peek());
 
     }
 }
